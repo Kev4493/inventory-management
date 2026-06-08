ce558b6dd17e333e710a33c2c7f77485b9176e09
Kevin Wagner <kevin_wagner4493@web.de>
2026-06-08 07:42:36 +0200

    [FEATURE] add dialog to create new employees

---
+++ src/components/CreateEmployeeDialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card flex justify-center">
+    <Dialog
+      v-model:visible="isOpen"
+      modal
+      header="Neuer Mitarbeiter"
+      :style="{ width: '50rem' }"
+    >
+      <EmployeeForm></EmployeeForm>
+    </Dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Dialog from 'primevue/dialog';
+import EmployeeForm from '@/components/EmployeeForm.vue';
+
+// Verbindet isOpen mit v-model:isOpen aus der Parent-Komponente.
+// Dadurch hat isOpen ein 'true' oder 'false' und der Dialog wird angezeigt oder nicht.
+const isOpen = defineModel<boolean>('isOpen');
+</script>
